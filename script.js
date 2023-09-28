e79f0d8bcfc88f0b3f7d8f8a96d02e35e83a852f
Jiwaji P <parabjiwaji@gmail.com>
2023-09-28 20:10:19 +0530

    bug fixes and code refactoring

--- script.js
+++ script.js
@@ -1,45 +1,3 @@
-/**
-If you haven’t already, set up your project with skeleton HTML/CSS and JS files.
-All of your book objects are going to be stored in a simple array,
-so add a function to the script (not the constructor) that can take user’s input and
-store the new book objects into an array. Your code should look something like this:
-
-const myLibrary = [];
-
-function Book() {
-  // the constructor...
-}
-
-function addBookToLibrary() {
-  // do stuff here
-}
-
-Write a function that loops through the array and displays each book on the page.
-You can display them in some sort of table, or each on their own “card”.
-It might help for now to manually add a few books to your array so you can see the display.
-
-Add a “NEW BOOK” button that brings up a form allowing users to input the details for the 
-new book: author, title, number of pages, whether it’s been read and anything else you might want.
-How you decide to display this form is up to you. For example, you may wish to have a form show in
-a sidebar or you may wish to explore dialogs and modals using the <dialog> tag. However you do this,
-you will most likely encounter an issue where submitting your form will not do what you expect it to do.
-That’s because the submit input tries to send the data to a server by default. If you’ve done the bonus
-section for the calculator assignment, you might be familiar with event.preventDefault();.
-Read up on the event.preventDefault documentation again and see how you can solve this issue!
-
-Add a button on each book’s display to remove the book from the library.
-
-You will need to associate your DOM elements with the actual book objects in some way.
-One easy solution is giving them a data-attribute that corresponds to the index of the library array.
-
-Add a button on each book’s display to change its read status.
-
-To facilitate this you will want to create the function that toggles a book’s read status on your Book prototype instance.
-NOTE: You’re not required to add any type of storage right now.
-You will have the option to come back to this project later on in the course.
-
-**/
-
 const submit = document.getElementById('addBook')
 const showForm = document.getElementById('showForm')
 const addBookDialog = document.getElementById('addBookDialog')
@@ -55,12 +13,16 @@ function Book(title, author, read) {
   this.title = title
   this.author = author
   this.read = read
+
+  this.toggleRead = () => {
+    this.read = !this.read
+  }
 }
 
 function addBookToLibrary(title = '', author = '', read = false) {
   if (title !== '' && author !== '') {
     const newBook = new Book(title, author, read)
-    console.log(newBook)
+    console.log(newBook, read)
     myLibrary.push(newBook)
     localStorage.setItem("myLibrary", JSON.stringify(myLibrary))
     getAllBooks()
@@ -74,8 +36,8 @@ function removeBookFromLibrary(index) {
   getAllBooks()
 }
 
-function updateBookRead(index, value) {
-  myLibrary[index].read = value
+function updateBookRead(index) {
+  myLibrary[index].toggleRead()
   localStorage.setItem("myLibrary", JSON.stringify(myLibrary))
   getAllBooks()
 }
@@ -101,7 +63,7 @@ const getAllBooks = () => {
       checkbox.checked = true
     }
     checkbox.addEventListener('change', (e) => {
-      updateBookRead(index, e.target.checked)
+      updateBookRead(index)
     })
     read.appendChild(checkbox)
 
@@ -130,8 +92,7 @@ showForm.addEventListener('click', () => {
 
 submit.addEventListener('click', (e) => {
   e.preventDefault()
-  const readValue = read.value === false ? false : true;
-  addBookToLibrary(title.value, author.value, readValue)
+  addBookToLibrary(title.value, author.value, read.checked)
   addBookDialog.close()
   reset()
 })
@@ -139,7 +100,7 @@ submit.addEventListener('click', (e) => {
 function reset() {
   title.value = ''
   author.value = ''
-  read.value = false
+  read.checked = false
 }
 
 
